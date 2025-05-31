078774fcac1bbfdf685e988f0bef90a151e379eb
XinoXz-12 <jaimelozanolozano12@gmail.com>
2025-05-31 18:15:55 +0200

    updated allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
-    allowedHosts: ['frontend'],
+    allowedHosts: ['frontend', 'ec2-34-203-202-63.compute-1.amazonaws.com'],
   }
 })
 

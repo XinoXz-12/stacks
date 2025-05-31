abaca8616b9651363c19c19ce6d310264c02528f
XinoXz-12 <jaimelozanolozano12@gmail.com>
2025-05-31 19:44:19 +0200

    updated services in frontend for temporal log

--- frontend/src/services/stacks.js
+++ frontend/src/services/stacks.js
@@ -15,6 +15,7 @@ const request = async (
 ) => {
     let url = `${BASE_URL}${endpoint}`;
     if (params) url += `?${new URLSearchParams(params)}`;
+    console.log(BASE_URL);
 
     const response = await fetch(url, {
         method,

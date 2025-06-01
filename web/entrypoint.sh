8ef90ee19f3a1e31e47ac34e6c05d20309253772
XinoXz-12 <jaimelozanolozano12@gmail.com>
2025-06-01 19:13:02 +0200

    updated the entrypoint to stop engine from starting with out certs

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -e
 
-echo "Iniciando NGINX con configuración HTTPS..."
+CERT_PATH="/etc/letsencrypt/live/stacks-gg.duckdns.org/fullchain.pem"
+
+if [ ! -f "$CERT_PATH" ]; then
+    echo "No hay certificado, generando dummy..."
+    mkdir -p /etc/letsencrypt/live/stacks-gg.duckdns.org
+    openssl req -x509 -nodes -newkey rsa:2048 \
+        -days 1 \
+        -keyout /etc/letsencrypt/live/stacks-gg.duckdns.org/privkey.pem \
+        -out /etc/letsencrypt/live/stacks-gg.duckdns.org/fullchain.pem \
+        -subj "/CN=localhost"
+fi
+
+echo "Iniciando NGINX..."
 nginx -g "daemon off;"

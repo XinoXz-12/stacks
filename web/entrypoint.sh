25937a6ce4cc3028e1d995aa3e11fca0884b984e
XinoXz-12 <jaimelozanolozano12@gmail.com>
2025-06-01 17:26:36 +0200

    updated new logic for certs

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,29 +1,5 @@
 #!/bin/sh
 set -e
 
-CERT_PATH="/etc/letsencrypt/live/stacks-gg.duckdns.org/fullchain.pem"
-NGINX_CONF_DIR="/etc/nginx/conf.d"
-
-if [ ! -f "$CERT_PATH" ]; then
-    echo "No hay certificado. Usando configuración temporal HTTP..."
-    cp /etc/nginx/http-only.conf $NGINX_CONF_DIR/default.conf
-else
-    echo "Certificado encontrado. Usando configuración HTTPS..."
-    cp /etc/nginx/default.conf $NGINX_CONF_DIR/default.conf
-fi
-
-echo "Iniciando NGINX..."
-nginx &
-
-if [ ! -f "$CERT_PATH" ]; then
-    echo "Esperando a que Certbot genere el certificado..."
-    while [ ! -f "$CERT_PATH" ]; do
-        sleep 5
-    done
-    echo "Certificado generado. Activando HTTPS..."
-
-    cp /etc/nginx/default.conf $NGINX_CONF_DIR/default.conf
-    nginx -s reload
-fi
-
-tail -f /var/log/nginx/access.log
+echo "Iniciando NGINX con configuración HTTPS..."
+nginx -g "daemon off;"
